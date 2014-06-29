cf482ac840ec83e0d62913a840d74ee6200bbd19
Jeremy Priestner <jmpriestner@gmail.com>
2014-06-28 21:29:03 -0400

    ps3 initial commit

---
+++ ps3/src/calculator/Parser.java
@@ -0,0 +1,64 @@
+package calculator;
+
+import calculator.Lexer;
+
+/*
+ * TODO define your grammar from problem 1 here
+ */
+
+/**
+ * Calculator parser. All values are measured in pt.
+ */
+class Parser {
+	
+	@SuppressWarnings("serial")
+	static class ParserException extends RuntimeException {
+	}
+
+	/**
+	 * Type of values.
+	 */
+	private enum ValueType {
+		POINTS, INCHES, SCALAR
+	};
+
+	/**
+	 * Internal value is always in points.
+	 */
+	public class Value {
+		final double value;
+		final ValueType type;
+
+		Value(double value, ValueType type) {
+			this.value = value;
+			this.type = type;
+		}
+
+		@Override
+		public String toString() {
+			switch (type) {
+			case INCHES:
+				return value / PT_PER_IN + " in";
+			case POINTS:
+				return value + " pt";
+			default:
+				return "" + value;
+			}
+		}
+	}
+
+	private static final double PT_PER_IN = 72;
+	//TODO check if the following is correct way to initialize
+	private final Lexer lexer = null;
+
+	// TODO write method spec
+	Parser(Lexer lexer) {
+		// TODO implement for Problem 3
+	}
+
+	// TODO write method spec
+	public Value evaluate() {
+		// TODO implement for Problem 3
+		return null;
+	}
+}
